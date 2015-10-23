c13d5cd79af14ef974e913d928cf68390d87c254
cstanca1 <cstanca1@yahoo.com>
2015-10-23 11:26:33 -0400

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## One function caching the matrix inverse and the other function retrieve it in a later run.
 
-## Write a short comment describing this function
-
+## Matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    mat.inv  <- NULL 
+    set  <- function(y){ 
+      x <<- y 
+      mat.inv <<- NULL  
+    } 
+    get  <- function() x 
+    setinverse  <- function(inverse) mat.inv <<- inverse 
+    getinverse  <- function() mat.inv 
+    list(set= set, get = get, setinverse = setinverse, getinverse = getinverse) 
 }
 
 
-## Write a short comment describing this function
-
+## Retrieve the calculated inverse from cache when matrix did not change
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    ## Matrix that is the inverse of x
+    mat.inv  <- x$getinverse() 
+    if (!is.null(mat.inv)){ 
+      message("getting cached matrix inverse") 
+      return(mat.inv) 
+    } 
+    data  <- x$get() 
+    mat.inv  <- solve(data, ...) 
+    x$setinverse(mat.inv) 
+    mat.inv 
 }
+
+
+## Test:
+## > x = rbind(c(1, 2), c(3, 4)) 
+## > mat = makeCacheMatrix(x)
+## > mat$get()
+## [,1] [,2]
+## [1,]    1    2
+## [2,]    3    4
+## first run - no cache
+## > cacheSolve(mat)
+## [,1] [,2]
+## [1,] -2.0  1.0
+## [2,]  1.5 -0.5
+## second run - from cache
+## > cacheSolve(mat) 
+## getting cached matrix inverse
+## [,1] [,2]
+## [1,] -2.0  1.0
+## [2,]  1.5 -0.5
